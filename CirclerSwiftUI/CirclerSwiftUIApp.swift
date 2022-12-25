e149b20dc660f96d2ef682d87467393f11d6c791
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-12-25 15:06:54 +0300

    Initial Commit

---
+++ CirclerSwiftUI/CirclerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclerSwiftUIApp.swift
+//  CirclerSwiftUI
+//
+//  Created by Mustafa Ali KILCI on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CirclerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
